0bbb876f1e68807a5565be79cb6dfa4259b4f45c
zhou chang <290621352@qq.com>
2018-03-16 15:06:56 +0800

    filesystemController add store mkdir

--- app/Http/Controllers/FileSystemController.php
+++ app/Http/Controllers/FileSystemController.php
@@ -8,8 +8,11 @@ class FileSystemController extends Controller
 {
     public function upload(Request $request)
     {
-        $path = $request->file('avatar')->store('avatars','public');
+        !is_dir(storage_path('app/public/avatar/' . date('Ymd'))) && mkdir(storage_path('app/public/avatar/' . date('Ymd')), 0777,true);
 
-        return '/storage/'.$path;
+        $dirPath = 'avatar/' . date('Ymd');
+        $path = $request->file('avatar')->store($dirPath, 'public');
+
+        return '/storage/' . $path;
     }
 }
